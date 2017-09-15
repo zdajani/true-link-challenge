0c3905736e5555791325e89cfb25d39540328509
zdajani <dajani.zeina@gmail.com>
2017-09-15 11:37:50 -0400

    clean up rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -30,14 +31,13 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
+  # Includes factory girl methods to tests
   config.include FactoryGirl::Syntax::Methods
-  config.include ApiHelper, :type=>:api
+
+  # includes json parser for tests
   config.include Requests::JsonHelpers, type: :request
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
@@ -57,16 +57,11 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
-    # Choose a test framework:
     with.test_framework :rspec
-
-    # Choose one or more libraries:
     with.library :active_record
     with.library :active_model
     with.library :action_controller
--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -1,7 +1,7 @@
-module ApiHelper  
+module ApiHelper
   include Rack::Test::Methods
 
   def app
     Rails.application
   end
-end  
+end
