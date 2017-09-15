e3af0bf31eeb39db1146020f0e9f6c39fcde5bf6
zdajani <dajani.zeina@gmail.com>
2017-09-15 15:29:01 -0400

    modified date calc to take start date instead of day

--- lib/calculate_date.rb
+++ lib/calculate_date.rb
@@ -1,26 +1,26 @@
 require 'active_support/time'
 
 class CalculateDate
-  def next(recurrance_num, day, dates_num)
-    dates_num == 1 ? first_date(day) : all_dates(recurrance_num, day, dates_num)
+  def next(recurrance_num, start_date, dates_num)
+    dates_num == 1 ? first_date(start_date) : all_dates(recurrance_num, start_date, dates_num)
   end
 
   private
 
-  def first_date(day)
-    year = Date.today.year
-    month = date_passed?(day) ? Date.today.month.next : Date.today.month
-    Date.new(year, month, day)
+  def first_date(start_date)
+    year = start_date.year
+    month = date_passed?(start_date) ? start_date.month.next : start_date.month
+    Date.new(year, month, start_date.day)
   end
 
-  def date_passed?(day)
-    Date.today.day > day
+  def date_passed?(start_date)
+    Date.today > start_date
   end
 
-  def all_dates(recurrance_num, day, dates_num)
-    dates = [first_date(day)]
+  def all_dates(recurrance_num, start_date, dates_num)
+    dates = [first_date(start_date)]
     (1..dates_num - 1).each do |num|
-      dates << first_date(day).next_month(num + (recurrance_num - 1))
+      dates << first_date(start_date).next_month(num + (recurrance_num - 1))
     end
     dates
   end
--- spec/lib/calculate_date_spec.rb
+++ spec/lib/calculate_date_spec.rb
@@ -3,31 +3,32 @@ require 'calculate_date'
 RSpec.describe CalculateDate do
   let(:month) { Date.today.month }
   let(:year) { Date.today.year }
-  let(:day) { 19 }
+  let(:start_date) { Date.today }
+  let(:day) { Date.today.day }
 
   describe 'next' do
     subject { CalculateDate.new }
 
     it 'calculates the next date' do
       recurrance_num = 1
-      expect(subject.next(recurrance_num, day, 1)).to eq(Date.new(year, month, day))
+      expect(subject.next(recurrance_num, start_date, 1)).to eq(Date.new(year, month, day))
     end
 
     it 'calculates the next 2 dates every 2 months' do
       recurrance_num = 2
       dates_num = 2
       expected_dates = [Date.new(year, month, day), Date.new(year, month + 2, day)]
-      expect(subject.next(recurrance_num, day, dates_num)).to eq(expected_dates)
+      expect(subject.next(recurrance_num, start_date, dates_num)).to eq(expected_dates)
     end
 
     it 'return the next date if start day is after current date' do
       recurrance_num = 1
       dates_num = 3
-      start_day = Date.today.prev_week(:friday).day
+      start_day = Date.today.prev_week(:friday)
       expected_dates = [
-        Date.new(year, month + 1, start_day),
-        Date.new(year, month + 2, start_day),
-        Date.new(year, month + 3, start_day)
+        Date.new(year, month + 1, start_day.day),
+        Date.new(year, month + 2, start_day.day),
+        Date.new(year, month + 3, start_day.day)
       ]
       expect(subject.next(recurrance_num, start_day, dates_num)).to eq(expected_dates)
     end
